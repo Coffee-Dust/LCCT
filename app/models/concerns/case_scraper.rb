2dded5854ee06ddf29cd8d7db68e4f57e6da6836
github username <github email address>
2020-07-27 13:46:49 -0400

    feat: add working case data scraper

--- app/models/concerns/case_scraper.rb
+++ app/models/concerns/case_scraper.rb
@@ -1,6 +1,31 @@
-require 'open-uri'
-require 'nokogiri'
-
 module CaseScraper
+
+  def get_new_data
+    data = scrape
+
+    raise ScraperMissingDataError if missing_data?(data)
+
+    return data
+  end
+
+  private
+  def scrape
+    #: total, active, deaths F
+    doc = Nokogiri::HTML(open("https://covid.knoxcountytn.gov/includes/covid_cases.csv"))
+    data_hash = {}
+    content = doc.css("p").text
+
+    raise ScraperNoDataError if content == "" || nil
+
+    data_hash[:total] = content.match(/Number of Cases\*,([0-9]+)/i)[1]
+    data_hash[:active] = content.match(/Active Cases,([0-9]+)/i)[1]
+    data_hash[:deaths] = content.match(/Deaths,([0-9]+)/i)[1]
+
+    return data_hash
+  end
+
+  def missing_data?(hash)
+    hash.collect { |key, value| value }.include?(nil)
+  end
 
 end
