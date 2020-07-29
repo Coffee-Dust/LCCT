1a9f929155b9bb034eaca780f989f1dcff005013
github username <github email address>
2020-07-29 17:49:40 -0400

    fix: #scrape now converts data into integers - fix #missingdata to see 0 as missing value

--- app/models/concerns/case_scraper.rb
+++ app/models/concerns/case_scraper.rb
@@ -17,15 +17,16 @@ module CaseScraper
 
     raise ScraperNoDataError if content == "" || nil
 
-    data_hash[:total] = content.match(/Number of Cases\*,([0-9]+)/i)[1]
-    data_hash[:active] = content.match(/Active Cases,([0-9]+)/i)[1]
-    data_hash[:deaths] = content.match(/Deaths,([0-9]+)/i)[1]
+    data_hash[:total] = content.match(/Number of Cases\*,([0-9]+)/i)[1].to_i
+    data_hash[:active] = content.match(/Active Cases,([0-9]+)/i)[1].to_i
+    data_hash[:deaths] = content.match(/Deaths,([0-9]+)/i)[1].to_i
 
     return data_hash
   end
 
   def missing_data?(hash)
-    hash.collect { |key, value| value }.include?(nil)
+    #convert the value to nil if its == 0 and collect the value
+    hash.collect { |key, value| value = nil if value == 0; value }.include?(nil)
   end
 
 end
