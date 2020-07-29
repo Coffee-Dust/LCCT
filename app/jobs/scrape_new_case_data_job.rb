4e99084d2a9ce0e1a6a3a3174b2f86354d621ef0
github username <github email address>
2020-07-29 12:14:42 -0400

    add scrape data jobs

---
+++ app/jobs/scrape_new_case_data_job.rb
@@ -0,0 +1,7 @@
+class ScrapeNewCaseDataJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ test/jobs/scrape_new_case_data_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ScrapeNewCaseDataJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
