9dbe3d74a8289bbb3ca729a864e240ed0f999c59
github username <github email address>
2020-07-30 11:56:04 -0400

    feat: #check_for_new_data - working

--- app/models/cases.rb
+++ app/models/cases.rb
@@ -1,3 +1,30 @@
 class Cases < ActiveRecord::Base
-  
+  validates_presence_of :total, :active, :deaths
+  def self.check_for_new_data
+    # scrape and instantiate new case > compare last db entry > if total is different ANDOR timestamp is next day create new entry
+    # if total is the same and hours have pasted from 11am then go ahead and update
+    new_case = Cases.new(CaseScraper::get_new_data)
+
+    return new_case.save if !Cases.first
+
+    last_entry = Cases.last; 
+
+    if last_entry.created_at.to_date == Time.now.to_date.yesterday
+      if Time.now > Time.parse("11:00 am")
+        if new_case.total == last_entry.total
+          # Saves new case if multiple hours have passed 
+          # in case the updated total is unchanged
+          return new_case.save if Time.now > Time.parse("3:00 pm")
+          # Otherwise assume data has not been updated
+          return false
+        elsif new_case.total > last_entry.total
+          new_case.save
+          return true
+        end
+      end
+    end
+    #all else failed
+    return false
+  end
+
 end
