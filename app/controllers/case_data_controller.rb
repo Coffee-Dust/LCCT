a85604a6e6fd9c08ddafe3887eaba58d8adbe530
github username <d24dust@hotmail.com>
2020-08-03 12:09:40 -0400

    feat: #overview renders data

--- app/controllers/case_data_controller.rb
+++ app/controllers/case_data_controller.rb
@@ -1,7 +1,16 @@
 class CaseDataController < ApplicationController
 
   def overview
-    
+    begin
+      if Cases.entry_created_today?
+        render plain: CaseUtils::InformativeResponse.overview
+      else
+        render plain: "The data has not been updated for today. \n#{CaseUtils::InformativeResponse.overview}"
+      end
+    rescue => error
+      render plain: "Server Error 569: #{error}"
+    end
+
   end
 
 end
