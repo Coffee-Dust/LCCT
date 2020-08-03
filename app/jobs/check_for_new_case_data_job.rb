ede8153d51025c43c4700ce85435caf00f6ba467
github username <d24dust@hotmail.com>
2020-08-03 11:29:50 -0400

    fix: will not fetch case was created for the day

--- app/jobs/check_for_new_case_data_job.rb
+++ app/jobs/check_for_new_case_data_job.rb
@@ -4,14 +4,21 @@ class CheckForNewCaseDataJob < ApplicationJob
     if @@status
       time = Time.parse("#{DateTime.now.tomorrow.to_date} 11am")
       self.class.set(wait_until: time).perform_later(job.arguments.first)
+      puts "ACTIVEJOBS: Scheduling to check again at 11am tomorrow!"
     else
-      self.class.set(wait_until: 5.minutes).perform_later(job.arguments.first)
+      self.class.set(wait_until: Time.now + 5.minutes).perform_later(job.arguments.first)
+      puts "ACTIVEJOBS: Case data not yet updated. Trying again in 5 mins..."
     end
   end
 
   def perform(yar)
     # Do something later
     @@status = Cases.check_for_new_data
-    puts @@status
+    puts "ACTIVEJOBS: The status for Cases.check_for_new_data called from job is: #{@@status}"
+    # The case check already added a entry for the day
+    if Cases.last.created_at.to_date == Time.now.to_date
+      puts "ACTIVEJOBS: The last case was created today already!"
+      @@status = true
+    end
   end
 end
