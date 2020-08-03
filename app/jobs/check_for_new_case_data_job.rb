0e83eb86f6d68adb123be85e289df9ecb4af84f6
github username <d24dust@hotmail.com>
2020-08-03 12:08:25 -0400

    add: time.now stamp to log outputs

--- app/jobs/check_for_new_case_data_job.rb
+++ app/jobs/check_for_new_case_data_job.rb
@@ -4,20 +4,20 @@ class CheckForNewCaseDataJob < ApplicationJob
     if @@status
       time = Time.parse("#{DateTime.now.tomorrow.to_date} 11am")
       self.class.set(wait_until: time).perform_later(job.arguments.first)
-      puts "ACTIVEJOBS: Scheduling to check again at 11am tomorrow!"
+      puts "#{Time.now} - ACTIVEJOBS: Scheduling to check again at 11am tomorrow!"
     else
       self.class.set(wait_until: Time.now + 5.minutes).perform_later(job.arguments.first)
-      puts "ACTIVEJOBS: Case data not yet updated. Trying again in 5 mins..."
+      puts "#{Time.now} - ACTIVEJOBS: Case data not yet updated. Trying again in 5 mins..."
     end
   end
 
   def perform(yar)
     # Do something later
     @@status = Cases.check_for_new_data
-    puts "ACTIVEJOBS: The status for Cases.check_for_new_data called from job is: #{@@status}"
+    puts "#{Time.now} - ACTIVEJOBS: The status for Cases.check_for_new_data called from job is: #{@@status}"
     # The case check already added a entry for the day
     if Cases.last.created_at.to_date == Time.now.to_date
-      puts "ACTIVEJOBS: The last case was created today already!"
+      puts "#{Time.now} - ACTIVEJOBS: The last case was created today already!"
       @@status = true
     end
   end
