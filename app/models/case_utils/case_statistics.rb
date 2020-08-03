63820f3584616e33a023e360bd6a4cf891d2cfed
github username <d24dust@hotmail.com>
2020-08-03 11:57:05 -0400

    create: methods

--- app/models/case_utils/case_statistics.rb
+++ app/models/case_utils/case_statistics.rb
@@ -1,14 +1,34 @@
-module CaseStatistics
-  def new_cases
+module CaseUtils
+  module CaseStatistics
+    def new_cases
+      return calc_case_diff(Cases.last.id, Cases.last.id-1)
+    end
 
+    def previous_day_new_cases
+      return calc_case_diff(Cases.last.id-1, Cases.last.id-2)
+    end
+
+    def new_deaths
+      begin
+        current = Cases.last
+        last_entry = Cases.find(current.id-1)
+        return current.deaths - last_entry.deaths
+      rescue
+        return "<data unavailable>"
+      end
+    end
+
+    def calc_case_trend
+      #Gather new cases from the prevous days - If at least 4 or so in a row are more or less than the previous
+      #return that status
+    end
+
+    def calc_case_diff(case1ID, case2ID)
+      begin
+        return Cases.find(case1ID).total - Cases.find(case2ID).total
+      rescue
+        return "<data unavailable>"
+      end
+    end
   end
-
-  def new_deaths
-
-  end
-
-  def calc_case_trend
-
-  end
-
 end
