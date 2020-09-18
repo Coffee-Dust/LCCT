6db3881c57ab5198041df313cd4bf4c781c5f24a
github username <d24dust@hotmail.com>
2020-09-18 15:37:41 -0400

    delete: could not implement

--- lib/exceptions.rb
+++
@@ -1,7 +0,0 @@
-class ScraperNoDataError < StandardError
-  def message
-    "Tried to scrape usings a pattern that returned NULL: Recheck website struct"
-  end
-end
-
-class ScraperMissingDataError < StandardError; end
