b90cc75bb345e29badc6c7aa0373fe5e341d2345
github username <d24dust@hotmail.com>
2020-09-27 15:17:07 -0400

    route for case_data#force_check

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'LCCT/overview', to: 'case_data#overview'
   get 'LCCT/week', to: 'case_data#week_overview'
+  get 'LCCT/force_check', to: "case_data#force_check"
 end
