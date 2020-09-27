0e056311a5c32cf4aa5961ece96744214ccdfa3a
github username <d24dust@hotmail.com>
2020-09-27 15:15:06 -0400

    update: error reporting feature

--- app/models/case_utils/case_scraper.rb
+++ app/models/case_utils/case_scraper.rb
@@ -5,25 +5,35 @@ module CaseUtils
     def self.get_new_data
       data = scrape
 
-      raise ScraperMissingDataError if missing_data?(data)
+      ErrorReporter.report(ScraperMissingDataError) if missing_data?(data)
 
       return data
     end
 
     private
     def self.scrape
-      #: total, active, deaths F
+      #hash format: total, active, deaths F
       doc = Nokogiri::HTML(open("https://covid.knoxcountytn.gov/includes/covid_cases.csv"))
       data_hash = {}
       content = doc.css("p").text
 
-      raise ScraperNoDataError if content == "" || nil
+      #This block will rescue and report any regex/scraper issues
+      begin
 
-      data_hash[:total] = content.match(/Number of Confirmed Cases,([0-9]+)/i)[1].to_i
-      data_hash[:active] = content.match(/Number of Active Cases,([0-9]+)/i)[1].to_i
-      data_hash[:deaths] = content.match(/Deaths,([0-9]+)/i)[1].to_i
-      
-      return data_hash
+        #When the devs can't make up their mind if they want a * or not
+        if c = content.match(/Number of Confirmed Cases,([0-9]+)/i)
+          data_hash[:total] = c[1].to_i
+        else
+          data_hash[:total] = content.match(/Number of Confirmed Cases\*,([0-9]+)/i)[1].to_i
+        end
+        #-----------------#
+        data_hash[:active] = content.match(/Number of asjd Cases,([0-9]+)/i)[1].to_i
+        data_hash[:deaths] = content.match(/Deaths,([0-9]+)/i)[1].to_i
+
+        return data_hash
+      rescue => error
+        ErrorReporter.report(ScraperNoDataError)
+      end
     end
 
     def self.missing_data?(hash)
