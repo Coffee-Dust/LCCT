3eabc6167db5fe01c5ed6c40d3c49a0ddf0eceda
github username <d24dust@hotmail.com>
2020-09-27 15:16:42 -0400

    update: error handling logic & force check for NewCaseDataJob

--- app/controllers/case_data_controller.rb
+++ app/controllers/case_data_controller.rb
@@ -1,20 +1,30 @@
 class CaseDataController < ApplicationController
 
   def overview
-    begin
+    #Check if there are any active errors
+
+    if e = CaseUtils::ErrorReporter.active_errors?(clear: true)
+
+      render plain: "Internal Server Error 569: #{e.name}"
+
+    else
+
       if Cases.entry_created_today?
         render plain: CaseUtils::InformativeResponse.overview
       else
         render plain: "The data has not been updated for today. \n#{CaseUtils::InformativeResponse.overview}"
       end
-    rescue => error
-      render plain: "Server Error 569: #{error}"
-    end
 
+    end#endif error check
   end
 
   def week_overview
     render plain: CaseUtils::InformativeResponse.new_case_overview
+  end
+
+  def force_check
+    CheckForNewCaseDataJob.perform_later(nil)
+    redirect_to action: "overview"
   end
 
 end
